f75d84566afc37cbc7a81fd35c8998d3db0c8afa
Jim Tryon <tryonjf@gmail.com>
2018-04-09 00:59:50 -0700

    Set variables for project

--- designs.js
+++ designs.js
@@ -1,5 +1,10 @@
+// Declare variables 
+var height, width, submit, colorPicker, sizePicker, canvas;
+
 // Select color input
+var colorPicker = $("#colorPicker");
 // Select size input
+var sizePicker = $("#sizePicker");
 
 // When size is submitted by the user, call makeGrid()
 
