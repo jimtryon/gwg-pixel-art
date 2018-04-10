02fbeee18ed54a13742eda670b4c78b8a132847d
Jim Tryon <tryonjf@gmail.com>
2018-04-09 23:55:34 -0700

    Add submit event handler

--- designs.js
+++ designs.js
@@ -1,15 +1,32 @@
-// Declare variables 
-var height, width, submit, colorPicker, sizePicker, canvas;
-
 // Select color input
 var colorPicker = $("#colorPicker");
 // Select size input
 var sizePicker = $("#sizePicker");
 
 // When size is submitted by the user, call makeGrid()
+$("#buttonSubmit").on("click", function(event) {
+  event.preventDefault();
+  console.log("Listening for submit event");
 
-function makeGrid() {
+const height = $("#inputHeight").val();
+const width = $("#inputWidth").val();
 
-// Your code goes here!
+  console.log(height);
+  console.log(width);
 
+  makeGrid(height, width);
+});
+
+function makeGrid(height, width) {
+  // Build table up to desired height
+  for (var cols = 0; cols <= height; cols++) {
+    // Create a row
+    console.log("row" + cols);
+
+    // Up to desired width
+    for (var rows = 0; rows <= width; rows++) {
+
+        console.log("td" + rows);
+    }
+  }
 }
